b7b93fa195a1a600a62ba3fb6f8f149a4851e06c
airzplaine <airzplaine@gmail.com>
2021-08-24 13:16:38 +0900

    lesson01practices
    practice problems

---
+++ Lesson01_Practice/src/com/example/practice1/Prc01.java
@@ -0,0 +1,16 @@
+package com.example.practice1;
+
+public class Prc01 {
+
+	public static void main(String[] args) {
+		// Q1. 컴파일
+		// Q2. 객체지향
+		// Q3. 자바 컴파일러
+		// Q4. JVM
+		// Q5. JRE
+		// Q6. 
+		System.out.println("Baek Geon");
+
+	}
+
+}
---
+++ Lesson01_Practice/src/com/example/practice1/Prc02.java
@@ -0,0 +1,24 @@
+package com.example.practice1;
+
+public class Prc02 {
+
+	public static void main(String[] args) {
+		// Q1. 아니오
+		// Q2. 아니오
+		// Q3. 부동 소수점 방식
+		// Q4.
+		int num1 = 10;
+		double num2 = 2.0;
+		
+		System.out.println((int)(num1 + num2));
+		System.out.println((int)(num1 - num2));
+		System.out.println((int)(num1 * num2));
+		System.out.println((int)(num1 / num2));
+		
+		// Q5.
+		char geul = '\uae00';
+		System.out.println(geul);
+
+	}
+
+}
